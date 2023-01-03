09d3eb9469ceacec52efabafb19fbe3bc8a52884
jdustindev <jdustindev@gmail.com>
2023-01-03 10:01:05 -0500

    Add initial code

---
+++ mastermind.rb
@@ -0,0 +1,25 @@
+class MastermindGame
+    CODE_COLORS = ['R', 'O', 'Y', 'G', 'B', 'P']
+    KEY_COLORS = ['B', 'W']
+
+    def initialize()
+        @allowed_guesses = 10
+        @num_guesses = 0
+        @game_board = []
+    end
+
+    def play()
+        while @num_guesses < @allowed_guesses
+            play_round
+        end
+    end
+
+    def play_round()
+        puts "Enter code: "
+        guess = gets.split('')
+        puts guess
+    end
+end
+
+game = MastermindGame.new
+game.play
